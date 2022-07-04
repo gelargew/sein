1c3136b23ce419ded3644152b893e2258953ac05
unknown <gelargew@gmail.com>
2022-07-04 19:23:19 +0700

    added head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout/Layout'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Head>
+        <title>sein.</title>
+      </Head>
     </Layout>
   ) 
 }
